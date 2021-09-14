9e267416046ec0003e2c57dc00c728de8b72a1ec
Haoran <bofengco@foxmail.com>
2021-09-14 17:57:55 +0800

    Update run.py

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ for file in file_list:
         for paragraph in document.paragraphs:
             all_content = all_content + paragraph.text
 
-punctuations = [".", ",", ";", "(", ")", ":", "—", "[", "]", "/"]
+punctuations = ["\n", "\t", ".", ",", ";", "(", ")", ":", "—", "[", "]", "/", "–", "-"]
 numbers = [" 0 ", " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 "]
 letters = [" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h ", " i ", " j ", " k ", " l ", " m ", " n ", " o ", " p ", " q ", " r ", " s ", " t ", " u ", " v ", " w ", " x ", " y ", " z "]
 articles = [" the ", " a ", " an "]
@@ -23,7 +23,7 @@ auxiliaryverbs = [" be ", " am ", " are ", " is ", " was ", " were ", " being ",
 conjunctions = [" for ", " and ", " nor ", " but ", " or ", " yet ", " so "]
 #https://www.thefreedictionary.com/List-of-pronouns.htm
 pronouns = [" all ", " another ", " any ", " anybody ", " anyone ", " anything ", " as ", " aught ", " both ", " each ", " each other ", " either ", " enough ", " everybody ", " everyone ", " everything ", " few ", " he ", " her ", " hers ", " herself ", " him ", " himself ", " his ", " i ", " idem ", " it ", " its ", " itself ", " many ", " me ", " mine ", " most ", " my ", " myself ", " naught ", " neither ", " no one ", " nobody ", " none ", " nothing ", " nought ", " one ", " one another ", " other ", " others ", " ought ", " our ", " ours ", " ourself ", " ourselves ", " several ", " she ", " some ", " somebody ", " someone ", " something ", " somewhat ", " such ", " suchlike ", " that ", " thee ", " their ", " theirs ", " theirself ", " theirselves ", " them ", " themself ", " themselves ", " there ", " these ", " they ", " thine ", " this ", " those ", " thou ", " thy ", " thyself ", " us ", " we ", " what ", " whatever ", " whatnot ", " whatsoever ", " whence ", " where ", " whereby ", " wherefrom ", " wherein ", " whereinto ", " whereof ", " whereon ", " wherever ", " wheresoever ", " whereto ", " whereunto ", " wherewith ", " wherewithal ", " whether ", " which ", " whichever ", " whichsoever ", " who ", " whoever ", " whom ", " whomever ", " whomso ", " whomsoever ", " whose ", " whosever ", " whosesoever ", " whoso ", " whosoever ", " ye ", " yon ", " yonder ", " you ", " your ", " yours ", " yourself ", " yourselves "]
-years = [" 1990 ", " 1991 ", " 1992 ", " 1993 ", " 1994 ", " 1995 ", " 1996 ", " 1997 ", " 1998 ", " 1999 ", " 2000 ", " 2001 ", " 2002 ", " 2003 ", " 2004 ", " 2005 ", " 2006 ", " 2007 ", " 2008 ", " 2009 ", " 2010 ", " 2011 ", " 2012 ", " 2013 ", " 2014 ", " 2015 ", " 2016 ", " 2017 ", " 2018 ", " 2019 ", " 2020 "]
+years = ["1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"]
 months = [" jan ", " feb ", " mar ", " apr ", " may ", " june ", " july ", " aug ", " sep ", " oct ", " nov ", " dec "]
 
 all_content = all_content.lower()
@@ -48,7 +48,7 @@ for i in range(0, len(temp)):
 
 
 obj = Counter(all_bigram_list)
-temp = obj.most_common(1000)
+temp = obj.most_common(500)
 #word
 for i in range(0, len(temp)):
     print(temp[i][0])
